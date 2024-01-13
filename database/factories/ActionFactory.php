33b6a76cf02a2bceb33555554012ccbfc8aaaa1d
asmakrl <90398895+asmakrl@users.noreply.github.com>
2024-01-13 19:28:04 +0100

    Seeders

--- database/factories/ActionFactory.php
+++ database/factories/ActionFactory.php
@@ -6,7 +6,7 @@ use App\Models\Type;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Action>
  */
 class ActionFactory extends Factory
 {
